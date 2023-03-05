bee0458c98cd65b0dc239e9453b94068909930c8
Momo <moharryg@gmail.com>
2023-03-05 15:12:11 +0000

    New Bata Version
    When showing only the file name it now replaces %20 with a space
    
    Removed deprecated code

--- scripts/image-selection.js
+++ scripts/image-selection.js
@@ -1,11 +1,10 @@
-// fires when 
-Hooks.on("renderTokenConfig", function (config, html) {
-    console.log(config.token._id);
-});
+const FVTTVaildImageTypes = ["jpg", "jpeg", "png", "svg", "webp"]; // Array of vaild image file types for FVTT
+const FVTTVaildVideoTypes = ["webm", "mp4", "m4v"]; // Array of vaild video file types for FVTT
 
 // fires when the token HUD is called to be rended
 Hooks.on("renderTokenHUD", async function (hud, html, token) {
 
+    // Gets an array of images that the token can use and checks if they are 2 or more
     const images = await game.actors.get(token.actorId)?.getTokenImages() ?? [];
     if (images.length < 2) {
         return;
@@ -13,22 +12,40 @@ Hooks.on("renderTokenHUD", async function (hud, html, token) {
 
     console.log("%c Momo's Token Changer Rending HUD", "color: green; font-size: 15px");
 
+    // Gets the settings for if to display the image or the path
     const imageSelectionDisplay = game.settings.get("momos-token-changer", "imageSelectionDisplay");
 
-    //TODO clean up and fix for versons 12+
-    const imagesParsed = images.map(im => {
-        const split = im.split('/');
-        var extension = im.split('.');
-        extension = extension[extension.length - 1];
-        const img = ['jpg', 'jpeg', 'png', 'svg', 'webp'].includes(extension);
-        const vid = ['webm', 'mp4', 'm4v'].includes(extension);
-        return { route: im, name: split[split.length - 1], used: im === token.img, img, vid, type: img || vid };
-    });
-    //
+    const parsedImages = images.map(image => {
+        // Gets the file name at the end of the file path
+        let filePath = image.split("/");
+        let fileName = filePath[filePath.length - 1];
+        // replace the %20 with a space
+        fileName = fileName.replaceAll("%20", " ");
 
-    const wildcardDisplay = await renderTemplate("/modules/momos-token-changer/html/hud.html", { imageSelectionDisplay, imagesParsed });
+        // gets the file extension
+        let fileExtensions = image.split(".");
+        fileExtensions = fileExtensions[fileExtensions.length - 1];
+
+        // checks the file extension is of a vaild type for use in FVTT
+        let isVaildImageExtensions = FVTTVaildImageTypes.includes(fileExtensions);
+        let isVaildVideoExtensions = FVTTVaildImageTypes.includes(fileExtensions);
+
+        // returns all the data
+        return {
+            route: image,
+            name: fileName,
+            used: image === token.texture.src,
+            isVaildImageExtensions,
+            isVaildVideoExtensions,
+            type: isVaildImageExtensions || isVaildVideoExtensions
+        };
+    });
+
+    // Adds the HTML in for the new button and image options screen
+    const wildcardDisplay = await renderTemplate("/modules/momos-token-changer/html/hud.html", { imageSelectionDisplay, parsedImages });
 
     html.find("div.right").append(wildcardDisplay).click(function (event) {
+        // gets the stats of the button clicked to check them
         let activeButton, clickedButton, tokenButton;
         for (const button of html.find("div.control-icon")) {
             if (button.classList.contains("active")) activeButton = button;
@@ -36,6 +53,8 @@ Hooks.on("renderTokenHUD", async function (hud, html, token) {
             if (button.dataset.action === "momos-token-changer-selector") tokenButton = button;
         }
 
+        // checks to see if the button clicked is the button we made
+        // checks if the button is active; if not it sets it as active, otherwise it is it removes active
         if (clickedButton === tokenButton && activeButton !== tokenButton) {
             clickedButton.classList.add("active");
             html.find(".momos-token-changer-selector-wrap")[0].classList.add("active");
@@ -48,51 +67,13 @@ Hooks.on("renderTokenHUD", async function (hud, html, token) {
 
     });
 
+    // Finds all the buttons marked with the class "momos-token-changer-button-select" and adds a click event to them.
     const buttons = html.find(".momos-token-changer-button-select");
-
     buttons.map(function (button) {
         buttons[button].addEventListener("click", function (event) {
-
-            // Gets a array of all controlled tokens
-            //const controlled = canvas.tokens.controlled;
-            // Gets the token at the found index of the conrolled token that trigged the "renderTokenHUD" hook
-            //const index = controlled.findIndex(x => x.id == token._id);
-            //const tokenToChange = controlled[index];
-
-
-            //const dimensions = getTokenDimensions(tokenToChange, event.target.dataset.name);
+            // When the buttons is clicked it gets the file path to the image and updates the token with it
             const update = [{ _id: token._id, img: event.target.dataset.name}];
-
             canvas.scene.updateEmbeddedDocuments("Token", update);
         });
     });
 });
-
-// Update selected tokens to flip between a 1x1 or a 2x2 grid.
-
-const updates = [];
-for (let token of canvas.tokens.controlled) {
-    let newSize = (token.data.height == 1 && token.data.width == 1) ? 2 : 1;
-    updates.push({
-        _id: token.id,
-        height: newSize,
-        width: newSize
-    });
-};
-
-// use canvas.tokens.updateMany instead of token.update to prevent race conditions
-// (meaning not all updates will be persisted and might only show locally)
-canvas.scene.updateEmbeddedDocuments("Token", updates);
-
-
-const getTokenDimensions = (token, imgName) => {
-    const height = imgName.match(/_height(.*)_/)
-    const width = imgName.match(/_width(.*)_/)
-    const scale = imgName.match(/_scale(.*)_/)
-
-    return {
-        height: height ? parseFloat(height[1]) : token.height,
-        width: width ? parseFloat(width[1]) : token.width,
-        scale: scale ? parseFloat(scale[1]) : token.scale,
-    }
-}
